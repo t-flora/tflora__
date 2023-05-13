af6ebd2e1eed588657b0b92a2706f663030e652c
Tiago Flora <tiagomflora@gmail.com>
2023-05-12 20:28:11 -0700

    Fix getAllPosts undefined bug

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,55 +6,71 @@ import html from 'remark-html';
 
 const postsDir = path.join(process.cwd(), 'posts');
 
-export interface PostDataModel {
-    id: string,
-    date: string,
-    title: string,
-    contentHtml: string,
-}
-
 export function getPostFiles() {
-    return readdirSync(postsDir);
+    const files = readdirSync(postsDir);
+    console.log(files); // Files are being found
+    if (!files) {
+        throw new Error("No post files found.")
+    }
+    return files;
 }
     
-type PostField = {
+export type PostFields = {
     [key: string]: any,
+    content: string,
 }
 
-export function getPost(fileName: string, fields: []): PostField {
+export function getPost(fileName: string): PostFields {
     // load markdown files, extract name equal to slug, return data requested in fields
     const slug = fileName.replace(/\.md$/, "");
-    const fullPath = path.join(postsDir, `${fileName}`);
+    const fullPath = path.join(postsDir, `${slug}.md`);
 
     const fileContents = readFileSync(fullPath, 'utf-8');
     const { data, content } = matter(fileContents);
 
-    const postData: PostField = {}; // An empty object has type never ?
+    // const postData: PostField = {}; // An empty object has type never ?
     
-    fields.forEach((field) => {
-        switch(field) {
-            case "slug":
-                postData[field] = slug;
-                break;
-            case "content":
-                postData[field] = content;
-                break;
-            default:
-                if (typeof data[field] !== "undefined") {
-                    postData[field] = data[field];
-                }
-        }
-    });
+    // fields.forEach((field) => {
+    //     if (field === "slug") {
+    //         postData[field] = slug;
+    //     }
+    //     if (field === "content") {
+    //         postData[field] = content;
+    //     }
+    //     if (typeof field !== undefined) {
+    //         postData[field] = data[field]
+    //     }
+    // });
 
-    return postData;
+    // console.log(postData);
+    // if (Object.keys(postData).length == 0) {
+    //     throw new Error("No postData collected from matter")
+    // }
+    // console.log("Date fron within postData" + postData.date);
+    // console.log("Title fron within postData" + postData.title);
+    // console.log("This is matter data: " + data);
+    // console.log(content);
+
+    return {
+        ...data,
+        content,
+    };
 }
 
-export function getAllPosts(fields: []) {
-    const allSlugs = getPostFiles();
-    const allPosts = allSlugs.map((slug) => {
-        getPost(slug, fields);
+console.log("Testing getPost: " + getPost("nand2tetris.md").date);
+
+export function getAllPosts(): PostFields[] {
+    const allFiles = getPostFiles();
+    console.log("This is the call to getPostFiles from getAllPosts " + allFiles)
+    const allPosts = allFiles.map((filename) => {
+        return getPost(filename);
     })
-    .sort((postA, postB) => (  postA < postB ? 1 : -1 ));
+    console.log("This is allPosts after mapping allFiles to getPost " + allPosts)
+    // .sort((postA, postB) => ( postA.date < postB.date ? 1 : -1 ));
+
+    if (!allPosts) {
+        throw new Error("allPosts is empty, null, or undefined");
+    }
 
     return allPosts;
 }
