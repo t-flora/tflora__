68511daf661aa6268eaa583e0f9f73e81bb4b045
Tiago Flora <tiagomflora@gmail.com>
2023-05-18 19:53:08 -0700

    add spin to globe, constrain rotation, and block zooming

--- components/ThreeJS/GlobeScene.tsx
+++ components/ThreeJS/GlobeScene.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Globe from 'react-globe.gl';
 import * as topojson from 'topojson-client';
 import * as THREE from 'three';
@@ -10,19 +10,19 @@ const polygonsMaterial = new THREE.MeshLambertMaterial({ color: 'darkslategrey',
 const World: React.FC = () => {
   const [landPolygons, setLandPolygons] = useState<any[]>([]);
 
-  const locData = locations.map((item, idx) => {
+  const locData = locations.map((item, _) => {
     const { latitude: lat, longitude: lng, ...rest } = item
     return {
         ...item,
         lat,
         lng,
         maxR: 10,
-        propagationSpeed: (Math.random() - 0.5) * 20 + 1,
-        repeatPeriod: Math.random() * 2000 + 200
+        propagationSpeed: 5,
+        repeatPeriod: 250
     }
   })
 
-  const colorInterpolator = ( t: any ) => `rgba(255,100,50,${Math.sqrt(1-t)})`;
+  const colorInterpolator = ( t: number ) => `rgba(255,100,50,${Math.sqrt(1-t)})`;
 
   useEffect(() => {
     fetch('//unpkg.com/world-atlas/land-110m.json')
@@ -32,10 +32,35 @@ const World: React.FC = () => {
       });
   }, []);
 
+    const globeEl = useRef();
+
+    useEffect(() => {
+      const globe = globeEl.current;
+
+      // Auto-rotate
+      globe.controls().autoRotate = true;
+      globe.controls().autoRotateSpeed = 2.5;
+      globe.controls().enableZoom = false;
+
+    globe.controls().maxPolarAngle = -Math.PI / 3;
+    globe.controls().minPolarAngle = Math.PI / 2;
+    }, []);
+
+//   const rotationAxes = {
+//     x: 0,
+//     y: 0,
+//     z: 0,
+//   }
+
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center max-w-screen-md'>
         <Globe
+        ref={globeEl}
+        animateIn={false}
         backgroundColor="rgba(0,0,0,0)"
+        polygonsData={landPolygons}
+        polygonCapMaterial={polygonsMaterial}
+        polygonSideColor={() => 'rgba(0, 0, 0, 0)'}
         showGlobe={false}
         showAtmosphere={false}
         ringsData={locData}
@@ -43,9 +68,7 @@ const World: React.FC = () => {
         ringMaxRadius='maxR'
         ringPropagationSpeed='propagationSpeed'
         ringRepeatPeriod='repeatPeriod'
-        polygonsData={landPolygons}
-        polygonCapMaterial={polygonsMaterial}
-        polygonSideColor={() => 'rgba(0, 0, 0, 0)'}
+        // objectRotation={rotationAxes}
         />
     </div>
   );
