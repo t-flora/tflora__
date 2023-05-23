3deab96a854de6f7432e19cfec7303ec4a3cb489
Tiago Flora <tiagomflora@gmail.com>
2023-05-23 16:24:39 -0700

    add minimal hamburger menu button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,7 @@
+'use client';
 import Link from "next/link";
+import React, { useState } from "react";
+import { Icon } from '@iconify/react';
 // import { useRouter } from "next/navigation";
 
 interface NavLinkProps {
@@ -14,10 +17,7 @@ const NavLink: React.FC<NavLinkProps> = ({ label, href }) => {
         <div className="block py-3 rounded">
             <Link href={href}
                 className="px-4 py-2 border-2 min-w-[48px] min-h-[48px] border-transparent hover:border-gray-400"
-                // className={`nav__item ${
-                //     isActive ? "active" : ""
-                //     }`}
-            >
+                >
                 {label}
             </Link>
         </div>
@@ -25,14 +25,35 @@ const NavLink: React.FC<NavLinkProps> = ({ label, href }) => {
 }
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="p-2 mb-3 justify-around">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="md:hidden">
+                <button type="button" onClick={() => (setIsOpen(!isOpen))}>
+                    <Icon icon="cil:hamburger-menu" fontSize={32} color="#fff" />
+                </button>
+            </div>
+            <div className="hidden md:flex max-w-screen-xl flex-wrap items-center justify-between mx-auto p-4">
                 <NavLink label="home" href="./" />
                 <NavLink label="about" href="./about" />
                 <NavLink label="what you want here" href="./skills" />
                 <NavLink label="blog" href="./blog" />
             </div>
+
+            {isOpen && (
+            <div>
+                {/* <button type="button" onClick={() => setIsOpen(!isOpen)}>
+                    <Icon icon="heroicons-solid:x" fontSize={32} className="h-6 w-6 text-white" color="#fff" />
+                </button> */}
+                <div className="md:hidden max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <NavLink label="home" href="./" />
+                    <NavLink label="about" href="./about" />
+                    <NavLink label="what you want here" href="./skills" />
+                    <NavLink label="blog" href="./blog" />
+                </div>
+            </div>
+            )}
         </nav>
     );
 }
